033f414d4b257f837f050aacc3031408109cdb42
lje <51558411>
2021-03-13 19:09:54 +0800

    流程定义、流程实例demo

--- src/test/java/com/sun/lje/activiti/workflow/Part8_ProcessRuntime.java
+++ src/test/java/com/sun/lje/activiti/workflow/Part8_ProcessRuntime.java
@@ -2,6 +2,7 @@ package com.sun.lje.activiti.workflow;
 
 import com.sun.lje.activiti.SecurityUtil;
 import org.activiti.api.process.model.ProcessInstance;
+import org.activiti.api.process.model.builders.ProcessPayloadBuilder;
 import org.activiti.api.process.runtime.ProcessRuntime;
 import org.activiti.api.runtime.shared.query.Page;
 import org.activiti.api.runtime.shared.query.Pageable;
@@ -22,7 +23,7 @@ public class Part8_ProcessRuntime {
     // 获取流程实例
     @Test
     public void getProcessInstance() {
-        securityUtil.logInAs("bob");
+        securityUtil.logInAs("john");
         Page<ProcessInstance> processInstancePage = processRuntime
                 .processInstances(Pageable.of(0,100));
         System.out.println("流程实例数"+ processInstancePage.getTotalItems());
@@ -42,28 +43,43 @@ public class Part8_ProcessRuntime {
     // 启动流程实例
     @Test
     public void startProcessInstance() {
-
+        securityUtil.logInAs("bob");
+        ProcessInstance start = processRuntime.start(ProcessPayloadBuilder.start()
+                .withProcessDefinitionKey("Process_1")
+                .withName("第一个流程实例名称")
+                .withBusinessKey("自定义key").build());
 
     }
 
     // 删除流程实例
     @Test
     public void delProcessInstance() {
-
+        securityUtil.logInAs("john");
+        ProcessInstance start = processRuntime.delete(ProcessPayloadBuilder
+                .delete()
+                .withProcessInstanceId("0bbe0e3e-83eb-11eb-a52d-c809a8e4389d")
+                .build());
 
     }
 
     //挂起
     @Test
     public void suspendProcessInstance() {
-
-
+        securityUtil.logInAs("john");
+        ProcessInstance processInstance = processRuntime.suspend(ProcessPayloadBuilder
+                .suspend()
+                .withProcessInstanceId("0bbe0e3e-83eb-11eb-a52d-c809a8e4389d")
+                .build());
     }
 
     // 启动
     @Test
     public void resumeProcessInstance() {
-
+        securityUtil.logInAs("john");
+        ProcessInstance processInstance = processRuntime.resume(ProcessPayloadBuilder
+                .resume()
+                .withProcessInstanceId("0bbe0e3e-83eb-11eb-a52d-c809a8e4389d")
+                .build());
 
     }
 
